9670c68b56b87b7d7d0a64ec5123db09d1738082
mahan-noor <ismahanabey@gmail.com>
2021-05-31 00:02:24 +0300

    create a post image function that allow users to post images for it to be rated

--- rateapp/views.py
+++ rateapp/views.py
@@ -4,3 +4,21 @@ from django.shortcuts import render,redirect
 def home(request):
     posts = Post_all_posts()
     return render(request, 'home.html' ,{'posts':posts})
+
+
+def post_image(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            
+    else:
+        form = PostForm()
+        
+    try:
+        posts = Post.objects.all()
+    except Post.DoesNotExist:
+        posts = None
+    return render(request,'post_image.html', {'posts': posts, 'form': form})
