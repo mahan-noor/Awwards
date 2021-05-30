53ff7df8175e685c667a8863a36cd32d464db9e7
mahan-noor <ismahanabey@gmail.com>
2021-05-30 23:49:02 +0300

    create navabar file and base add a function home in views

--- rateapp/views.py
+++ rateapp/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+def home(request):
+    posts = Post_all_posts()
+    return render(request, 'home.html' ,{'posts':posts})
