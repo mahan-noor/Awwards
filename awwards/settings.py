e5cdafbe50a5a5ea20ed464b7fbff3dc8bf487d3
mahan-noor <ismahanabey@gmail.com>
2021-05-31 10:52:29 +0300

    add django static urls and media url and run migration

--- awwards/settings.py
+++ awwards/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -77,8 +78,10 @@ WSGI_APPLICATION = 'awwards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'awwards',
+        'USER': 'lenovo',
+        'PASSWORD':'1234',
     }
 }
 
@@ -124,6 +127,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- rateapp/urls.py
+++ rateapp/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
+from django.urls import path,re_path
 from . import views
 from django.conf import settings
-from django.conf.urls.static import static 
+from django.conf.urls.static import static
 
 urlpatterns=[
     path('',views.home, name='home'),
