2bf9cabaaa722bbad2813307e2ac3379af6cc4f1
mahan-noor <ismahanabey@gmail.com>
2021-05-31 09:56:23 +0300

    add path url of search

--- rateapp/urls.py
+++ rateapp/urls.py
@@ -5,5 +5,7 @@ from django.conf import settings
 urlpatterns=[
     path('',views.home, name='home'),
     path('new/project', views.new_project, name='new-project'),
+    path('search/', views.search_results, name='search_results'),
+
 
 ]
