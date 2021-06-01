48ed2638e593189221669919668de775795167fb
mahan-noor <ismahanabey@gmail.com>
2021-06-01 17:19:22 +0300

    add javascript on the navbar

---
+++ static/css/script/main.js
@@ -0,0 +1,17 @@
+// $(document).ready(function () {
+
+//     $('#sidebarCollapse').on('click', function () {
+//         $('#sidebar').toggleClass('active');
+//     });
+
+// });
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
