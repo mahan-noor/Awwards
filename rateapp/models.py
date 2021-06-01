b0178e57c647214f5afa4c7e7230d0ab8326351c
mahan-noor <ismahanabey@gmail.com>
2021-06-01 07:30:48 +0300

    create class projectin models and add method to get project and search the project

--- rateapp/models.py
+++ rateapp/models.py
@@ -8,9 +8,11 @@ from tinymce.models import HTMLField
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
     bio = HTMLField()
-    profile_pic = CloudinaryField(manual_crop ='1080x1080')
+    profile_pic = CloudinaryField('1080x1080')
     contact_info = models.CharField(max_length=144)
     
+    def __str__(self):
+        return self.content
 
 
     @classmethod
@@ -25,5 +27,29 @@ class Profile(models.Model):
         return 
 
 
+class Project(models.Model):
+    profile = models.ForeignKey(User, related_name="projects", on_delete=models.CASCADE)
+    title = models.CharField(max_length=144)
+    description = models.TextField()
+    img = CloudinaryField( '1920x1080')
+    live_site = models.URLField(max_length=250)
 
+
+    def __str__(self):
+        return self.content
+
+    @classmethod
+    def get_projects(cls):
+        projects = cls.objects.all()
+        return projects
+
+    @classmethod
+    def search_by_title(cls,search_term):
+        projects = cls.objects.filter(title__icontains=search_term)
+        return projects
+
+    @classmethod
+    def get_profile_projects(cls, profile):
+        projects = Project.objects.filter(profile__pk = profile)
+        return projects
 
