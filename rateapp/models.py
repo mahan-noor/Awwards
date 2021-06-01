e2bd1ac6b6724bba609c0a1682d87094e4b80550
mahan-noor <ismahanabey@gmail.com>
2021-06-01 07:12:23 +0300

    add class profile in the model and add tinymce in installed apps

--- rateapp/models.py
+++ rateapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from tinymce.models import HTMLField
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
+    bio = HTMLField()
+    profile_pic = CloudinaryField(manual_crop ='1080x1080')
+    contact_info = models.CharField(max_length=144)
