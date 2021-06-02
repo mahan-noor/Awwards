9a644b0063bda9eab437c9efed429cc718e156d2
mahan-noor <ismahanabey@gmail.com>
2021-06-02 07:08:12 +0300

    addlogin and signup template and fix bug on displaying login page

--- awwards/urls.py
+++ awwards/urls.py
@@ -15,9 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.contrib.auth import views
+from django.contrib.auth import views as auth_views
+from django_registration.backends.one_step.views import RegistrationView
+from django.contrib.auth.views import LogoutView, LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('rateapp.urls')),
+    path('accounts/register/',
+        RegistrationView.as_view(success_url='/email'),
+        name='django_registration_register'),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', auth_views.LogoutView.as_view()),  
+    path('accounts/login', LoginView.as_view(redirect_field_name ='/',success_url = '/'), name = 'login'),
+
 ]
